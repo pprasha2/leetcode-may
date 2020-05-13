a61dd127543d76c40f39254ff26437444b90e90a
prashant <prashant.sharma112@gmail.com>
2020-05-13 22:18:16 +0530

    day 13 commit

---
+++ day13.py
@@ -0,0 +1,44 @@
+'''
+Remove K Digits
+Solution
+Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+Note:
+The length of num is less than 10002 and will be ≥ k.
+The given num does not contain any leading zero.
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+'''
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        while k:
+            k -= 1
+            index = -1
+            n = len(num)
+            for i in range(n-1):
+                #print(i,num[i])
+                if int(num[i]) > int(num[i+1]):
+                    index = i
+                    num = num.replace(num[index],"",1)
+                    if index == 0:
+                        while num != "" and num[0] == "0":
+                            num = num.replace("0","",1)
+                    break
+            if index == -1 and num != "":
+                num = num.replace(num[-1],"",1)
+        if num == "":
+            return "0"
+        return num
