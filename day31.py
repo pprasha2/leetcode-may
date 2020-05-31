2b443c5e50d9d43751ffcb569a86399da8492628
prashant <prashant.sharma112@gmail.com>
2020-05-31 16:03:38 +0530

    day 31 commit

---
+++ day31.py
@@ -0,0 +1,46 @@
+'''
+Edit Distance
+Solution
+Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+Example 2:
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+'''
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = i
+        for i in range(n+1):
+            dp[0][i] = i
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if word2[i-1] == word1[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j]) + 1
+        return dp[m][n]
+
